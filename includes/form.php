a97426be754d0b0b3bbdf7d64b058985d307f742
unknown <christpher.elliot@44389-WEB-WIN.auckland.natcoll.ac.nz>
2013-06-14 14:57:15 +1200

    form validation

--- includes/form.php
+++ includes/form.php
@@ -3,44 +3,94 @@
 class Form {
 
 	private $sHTML;
+	private $aData;
+	private $aErrors;
 
 	public function __construct(){
 
 		$this->sHTML = '<form action="" method="post">';
+		$this->aData = array();
+		$this->aErrors = array();
 
 	}
 
 	public function makeInput($sControlName,$sLabel){
 
+		$sData = "";
+		if(isset($this->aData[$sControlName])){ 
+			$sData = $this->aData[$sControlName];
+		}
+
+		$sError = "";
+		if(isset($this->aErrors[$sControlName])){
+			$sError = $this->aErrors[$sControlName];
+		}
+
 		$this->sHTML .= '
 			<label for="'.$sControlName.'">'.$sLabel.':</label>
-            <input type="text" name="'.$sControlName.'" id="'.$sControlName.'" value=""/>
-			<div class="message"></div><div class="clear"></div>';
+            <input type="text" name="'.$sControlName.'" id="'.$sControlName.'" value="'.$sData.'"/>
+			<div class="message">'.$sError.'</div><div class="clear"></div>';
 
 	}
 
 	public function makeTextArea($sControlName,$sLabel){
 
+		$sData = "";
+		if(isset($this->aData[$sControlName])){ 
+			$sData = $this->aData[$sControlName];
+		}
+
+		$sError = "";
+		if(isset($this->aErrors[$sControlName])){
+			$sError = $this->aErrors[$sControlName];
+		}
+
 		$this->sHTML .= '
 			<label for="'.$sControlName.'">'.$sLabel.':</label>
-            <div class="message"></div><textarea rows="4" cols="42" name="'.$sControlName.'" id="'.$sControlName.'"></textarea>
+            <div class="message">'.$sError.'</div><textarea rows="4" cols="42" name="'.$sControlName.'" id="'.$sControlName.'">'.$sData.'</textarea>
 			<div class="clear"></div>';
 		
 	}
 
 	public function makePass($sControlName,$sLabel){
 
+		$sData = "";
+		if(isset($this->aData[$sControlName])){ 
+			$sData = $this->aData[$sControlName];
+		}
+
+		$sError = "";
+		if(isset($this->aErrors[$sControlName])){
+			$sError = $this->aErrors[$sControlName];
+		}
+
 		$this->sHTML .= '
 			<label for="'.$sControlName.'">'.$sLabel.':</label>
-            <input type="password" name="'.$sControlName.'" id="'.$sControlName.'" value=""/>
-			<div class="message"></div><div class="clear"></div>';
+            <input type="password" name="'.$sControlName.'" id="'.$sControlName.'" value="'.$sData.'"/>
+			<div class="message">'.$sError.'</div><div class="clear"></div>';
 
 	}
+
 
 	public function makeSubmit($sControlName,$sLabel){
 
 		$this->sHTML .= '<input name="'.$sControlName.'" id="'.$sControlName.'" type="submit" value="'.$sLabel.'" />
 						<div class="clear"></div>';	
+	}
+
+
+	public function checkRequired($sControlName){
+
+		$sData = "";
+
+		if(isset($this->aData[$sControlName])){
+			$sData = trim($this->aData[$sControlName]); 
+		}
+
+		if(strlen($sData)==0){
+			$this->aErrors[$sControlName] = "Required field"; 
+		}
+
 	}
 
 
@@ -51,8 +101,27 @@ class Form {
 			case 'html':
 				return $this->sHTML . "</form>";
 				break;
+			case 'valid':
+				if(count($this->aErrors)==0){
+					return true;
+				}else{
+					return false;
+				}
+				break;
 			default:
 				die($sProperty . " is restricted");
+		}
+	}
+
+
+	public function __set($sProperty,$value){
+
+		switch ($sProperty) {
+			case 'data':
+				$this->aData = $value;
+				break;	
+			default:
+				die($sProperty . " is unable to be written to");
 		}
 	}
 
--- register.php
+++ register.php
@@ -2,9 +2,43 @@
 
 require_once("includes/head.php");
 require_once("includes/form.php");
-
+require_once("includes/customer.php");
 
 $oForm = new Form();
+
+if(isset($_POST["submit"])){
+
+    $oForm->data = $_POST;  
+    $oForm->checkRequired("firstname");
+    $oForm->checkRequired("lastname");
+    $oForm->checkRequired("address");
+    $oForm->checkRequired("phone");
+    $oForm->checkRequired("email");
+    $oForm->checkRequired("username");
+    $oForm->checkRequired("password");
+    $oForm->checkRequired("confirmpassword");
+       
+
+      if($oForm->valid==true){
+
+            $oCustomer = new Customer();
+            $oCustomer->firstname = $_POST["firstname"];
+            $oCustomer->lastname = $_POST["lastname"];
+            $oCustomer->address = $_POST["address"];
+            $oCustomer->phone = $_POST["phone"];
+            $oCustomer->email = $_POST["email"];
+            $oCustomer->username = $_POST["username"];
+            $oCustomer->password = $_POST["password"];
+            $oCustomer->password = $_POST["confirmpassword"];
+            $oCustomer->save();
+
+      header("Location:index.php"); 
+      exit;
+
+      }
+
+} 
+
 $oForm->makeInput("firstname","First Name");
 $oForm->makeInput("lastname","Last Name");
 $oForm->makeTextArea("address","Address");
