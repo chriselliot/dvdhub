cde95e33c320c0b075de6006925b2aa7d68e4a18
Chris <chris@webtree.co.nz>
2013-06-25 20:56:37 +1200

    email

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 require_once("includes/head.php");
 require_once("includes/form.php");
 require_once("includes/customer.php");
+require_once("includes/class.phpmailer.php");
 
 $oForm = new Form();
 
@@ -24,7 +25,6 @@ if(isset($_POST["submit"])){
 
          $oForm->raiseCustomError("username","Username already taken");
     }
-       
 
     if($oForm->valid==true){
 
@@ -38,6 +38,22 @@ if(isset($_POST["submit"])){
             $oCustomer->password = $_POST["password"];
             $oCustomer->password = $_POST["confirmpassword"];
             $oCustomer->save();
+
+            $mail = new PHPMailer();
+            $mail->SetFrom('info@dvdhub.co.nz', 'DVD Hub administrator');
+            $mail->AddReplyTo('info@dvdhub.co.nz','DVD Hub administrator');
+            $mail->AddAddress($_POST["email"], $_POST["firstname"].$_POST[" lastname"]);
+            $mail->Subject = 'Welcome to DVD Hub';
+            $mail->MsgHTML('Your registration with DVD Hub was successful.');
+            $mail->AltBody = 'Your registration with DVD Hub was successful.';
+            $mail->AddAttachment('assets/images/logo.png');
+
+            //Send the message, check for errors
+            if(!$mail->Send()) {
+              echo "Mailer Error: " . $mail->ErrorInfo;
+            } else {
+              echo "Message sent!";
+            }
 
             header("Location:index.php"); 
             exit;
